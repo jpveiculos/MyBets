46d01988909f0e6e93314e65a80c53855209f269
MyBets <jp_veiculos@icloud.com>
2026-09-19 14:18:48 -0300

    Atualizar cache do Service Worker

--- backend/frontend/sw.js
+++ backend/frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="mybets-v1";
+const CACHE_NAME="mybets-v2";
 const APP_SHELL=["/","/index.html","/admin.html","/admin-manifest.json","/style.css"];
 
 self.addEventListener("install",event=>{
