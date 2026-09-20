39a0be94b91af32fc0bac942d5e76d906d36f4bc
MyBets <jp_veiculos@icloud.com>
2026-09-19 23:17:21 -0300

    Android: preparar versão 1.0.2 do APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.app.mybets"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.0.2"
     }
 
     compileOptions {
