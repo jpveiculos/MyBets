5b94aba6311f7cc13616d2aad038e015bb619109
MyBets <jp_veiculos@icloud.com>
2026-09-19 23:33:13 -0300

    Android: iniciar build final do APK 1.0.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.app.mybets"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
     }
 
     compileOptions {
