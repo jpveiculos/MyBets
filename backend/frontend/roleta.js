8455f3a0b7eb73ebcae000ddc9b8a4a48bbb7491
MyBets <jp_veiculos@icloud.com>
2026-09-24 12:38:04 -0300

    Aumentar levemente a fonte dos prêmios da roleta

--- backend/frontend/roleta.js
+++ backend/frontend/roleta.js
@@ -141,7 +141,7 @@ function drawWheel(){
     label.setAttribute("x",xy[0]);
     label.setAttribute("y",xy[1]);
     label.setAttribute("fill","#fff");
-    label.setAttribute("font-size","15");
+    label.setAttribute("font-size","17");
     label.setAttribute("font-family","Arial,Helvetica,sans-serif");
     label.setAttribute("font-weight","900");
     label.setAttribute("text-anchor","middle");
