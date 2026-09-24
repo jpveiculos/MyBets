c8d72e75d8f7999614d7e09bff222ce84649ce33
MyBets <jp_veiculos@icloud.com>
2026-09-24 19:45:02 -0300

    ui: set roulette prize labels to 18px

--- backend/frontend/roleta.js
+++ backend/frontend/roleta.js
@@ -141,7 +141,7 @@ function drawWheel(){
     label.setAttribute("x",xy[0]);
     label.setAttribute("y",xy[1]);
     label.setAttribute("fill","#fff");
-    label.setAttribute("font-size","19");
+    label.setAttribute("font-size","18");
     label.setAttribute("font-family","Arial,Helvetica,sans-serif");
     label.setAttribute("font-weight","900");
     label.setAttribute("text-anchor","middle");
